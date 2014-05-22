5751decc50b9c6a3133948eb91f344c3d86cd2b8
Michael Taylor Scheer <me@michaelscheer.com>
2014-05-22 14:05:47 -0400

    Add images, setup activity layout and began coding

--- COP4656-Assign2/src/com/michaelscheer/android/cop4656/assignment2/MainActivity.java
+++ COP4656-Assign2/src/com/michaelscheer/android/cop4656/assignment2/MainActivity.java
@@ -4,12 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageSwitcher;
 
 public class MainActivity extends Activity {
 
+	final ImageSwitcher imgSwitch = (ImageSwitcher)findViewById(R.id.imageSwitcher1);
+	private int switchPic = 0;
+	private final int[] images = { R.drawable.nole, R.drawable.koolgator };
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Initial Image of FSU
+        imgSwitch.setImageResource(images[switchPic]);
+       
+    
+    }
+
+
+    private void switchImage() {
+    	if (switchPic == 0)
+    		imgSwitch.setImageResource(images[++switchPic]);
+    	else
+    		imgSwitch.setImageResource(images[--switchPic]);
+    	
     }
 }
