cd4949cc9fb667105b4bf1035a781b83e4e9d155
Michael Taylor Scheer <me@michaelscheer.com>
2014-05-27 22:27:56 -0400

    Cleaned up code and added button animation

--- COP4656-Assign2/src/com/michaelscheer/android/cop4656/assignment2/MainActivity.java
+++ COP4656-Assign2/src/com/michaelscheer/android/cop4656/assignment2/MainActivity.java
@@ -2,22 +2,20 @@ package com.michaelscheer.android.cop4656.assignment2;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageSwitcher;
 import android.widget.ViewSwitcher;
 
 public class MainActivity extends Activity {
 
 	ViewSwitcher imgSwitch;
 	Button btnChangePic;
+	Animation vanish;
+	Animation appear;
 	
 	private int switchPic = 0;
-	int[] images = { R.drawable.nole, R.drawable.koolgator };
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +28,38 @@ public class MainActivity extends Activity {
     protected void init() {
     	imgSwitch = (ViewSwitcher)findViewById(R.id.viewSwitcher1);
     	btnChangePic = (Button)findViewById(R.id.button1);
+    	vanish = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+    	appear  = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
 
+    	// Animation idea from android tutorial
     	//Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.test);
     	//imgSwitch.startAnimation(hyperspaceJump);
     	
-    	btnChangePic.setText(R.string.showNole);
+    	// Set initial text
+    	btnChangePic.setText(R.string.showGator);
         btnChangePic.setOnClickListener(btnChangeListener);
     }
     
     View.OnClickListener btnChangeListener = new View.OnClickListener() {
-		
 		@Override
 		public void onClick(View v) {
 			switchImage();
-			
 		}
 	};
     
     private void switchImage() {
     	if (switchPic == 0) {
+    		btnChangePic.startAnimation(vanish);
     		imgSwitch.showNext();
+    		btnChangePic.setText(R.string.showNole);
+    		btnChangePic.startAnimation(appear);
     		switchPic = 1;
     	}
     	else {
+    		btnChangePic.startAnimation(vanish);
     		imgSwitch.showPrevious();
+    		btnChangePic.setText(R.string.showGator);
+    		btnChangePic.startAnimation(appear);
     		switchPic = 0;
     	}
     	
